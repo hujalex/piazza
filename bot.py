347e504262c76404ac496cdcc8f9580e5eeab8be
James Peng <jgpeng@umich.edu>
2024-11-16 17:21:00 -0500

    Basic prompting

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 import os
 
 from groq import Groq
+from dotenv import load_dotenv
 
+load_dotenv()
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-chat_completion = client.chat.completions.create(
+def prompt(prompt):
+    chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Explain the importance of fast language models",
+            "content": prompt,
         }
     ],
-    model="llama3-8b-8192",
-)
-
-print(chat_completion.choices[0].message.content)
+    model="llama3-70b-8192",
+    )
+    return chat_completion.choices[0].message.content
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from piazza_api import Piazza
 import os 
 from dotenv import load_dotenv
 from html2text import HTML2Text
+from bot import prompt
 
 HTML_2_TEXT = HTML2Text()
 HTML_2_TEXT.ignore_links = True
@@ -12,18 +13,20 @@ EMAIL = str(os.getenv("EMAIL"))
 PASSWORD = str(os.getenv("PASSWORD"))
 
 
-print(EMAIL)
 
 
 p = Piazza()
 
 p.user_login(email = EMAIL, password = PASSWORD)
 
-course = p.network("m08oofbzpg02wn") #  Add your own class id
+course = p.network("m09vgkjb1t96r0") #  Add your own class id
 
-posts = course.iter_all_posts(limit = 10)
+posts = course.iter_all_posts(limit = 40)
+prompts = ""
 for post in posts:
     post_content = post['history'][0]['content']
     post_content_as_text = HTML_2_TEXT.handle(post_content)
-    print(post_content_as_text)
-
+    prompts+=post_content_as_text+"\n\n\n"
+print(prompt("Classify the following posts into categories and write a summary: "+prompts))
+# print(prompt("Generate a list of frequently asked questions from the following questions:"+prompts))
+# print(prompt("Group similar posts in the following posts together: "+prompts))
